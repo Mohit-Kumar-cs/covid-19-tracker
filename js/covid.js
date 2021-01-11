a74e851ef0d5ee86b5b8f25911a8cf2f402e7828
Mohit-Kumar-cs <76441031+Mohit-Kumar-cs@users.noreply.github.com>
2021-01-11 22:01:46 +0530

    Add files via upload

---
+++ js/covid.js
@@ -0,0 +1,90 @@
+const APIURL = "https://disease.sh/v3/covid-19/countries/india";
+
+async function getRecords(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    console.log(resData);
+    showRecords(resData);
+}
+
+
+getRecords(APIURL);
+
+function showRecords(record) {
+    console.log(record);
+    document.getElementById("main").innerHTML =
+        `
+        <div class="container">
+
+        <h2 class="h2">Cases</h2>
+            <div class="cases">
+                <div class="cases-item">
+                <h3 class="h3">Total Cases</h3>
+                <div class="record">${record.cases} </div>
+            </div>
+                    <div class="cases-item">
+                        <h3 class="h3">Cases per Million</h3>
+                        <div class="record">${record.casesPerOneMillion}</div>
+                    </div>
+                    <div class="cases-item">
+                        <h3 class="h3">Cases reported Today</h3>
+                        <div class="record">${record.todayCases}</div>
+                    </div>
+                    </div>
+                    <hr class="hr">
+                    <h2 class="h2">Recovered</h2>
+                <div class="recovered">
+                    <div class="recovered-item">
+                        <h3 class="h3">Total Recovered</h3>
+                        <div class="record">${record.recovered} </div>
+                    </div>
+                    <div class="recovered-item">
+                        <h3 class="h3">Cases per Million</h3>
+                        <div class="record">${record.recoveredPerOneMillion}</div>
+                    </div>
+                    <div class="recovered-item">
+                        <h3 class="h3">Cases Recovered Today</h3>
+                        <div class="record">${record.todayRecovered}</div>
+                    </div>
+                </div>
+                <hr class="hr">
+                <h2 class="h2">Active Cases</h2>
+                <div class="active">
+                <div class="active-item">
+                        <h3 class="h3">Total Active Cases</h3>
+                        <div class="record">${record.active} </div>
+                        </div>
+                        <div class="active-item">
+                            <h3 class="h3">Active Cases per Million</h3>
+                            <div class="record">${record.activePerOneMillion}</div>
+                            </div>
+                </div>
+                <hr class="hr">
+                <h2 class="h2">Deaths</h2>
+                <div class="deaths">
+                <div class="deaths-item">
+                        <h3 class="h3">Total Deaths</h3>
+                        <div class="record">${record.deaths} </div>
+                    </div>
+                    <div class="deaths-item">
+                        <h3 class="h3">Deaths per Million</h3>
+                        <div class="record">${record.deathsPerOneMillion}</div>
+                    </div>
+                    <div class="deaths-item">
+                        <h3 class="h3">Deaths reported Today</h3>
+                        <div class="record">${record.todayDeaths}</div>
+                    </div>
+                </div>    
+                <hr class="hr">
+                <h2 class="h2">Updated</h2>
+                <div class="updated">
+                    <div class="updated-item">
+                        <h3 class="h3"></h3>
+                        <div class="record">${record.updated}</div>
+                    </div>
+                </div>
+        </div>   
+            `
+
+
+}
